52b20f049106833ba3d85454a3103b295ea635fc
Stenbyte <sten.scheifel@gmail.com>
2021-07-22 15:41:53 +0300

    texteditor draftjs

---
+++ src/components/textEditor.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import { Editor } from "react-draft-wysiwyg";
+import { EditorState } from "draft-js";
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+
+const TextEditor = () => {
+  const [editorState, setEditorState] = useState(EditorState.createEmpty());
+
+  const onEditorStateChange = (editorState) => {
+    setEditorState(editorState);
+  };
+  return (
+    <div
+      style={{
+        backgroundColor: "white",
+        height: "30vh",
+        width: "30vw",
+        margin: "0 auto",
+      }}
+    >
+      <Editor
+        editorState={editorState}
+        toolbarClassName="toolbarClassName"
+        wrapperClassName="wrapperClassName"
+        editorClassName="editorClassName"
+        onEditorStateChange={onEditorStateChange}
+      />
+    </div>
+  );
+};
+
+export default TextEditor;
